6c1dcd90edb05ade652148a84270969c85a6911c
Uranowski Antoni <antoni.uranowski@microsoft.wsei.edu.pl>
2019-10-06 10:29:19 +0200

    ex_1

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,12 @@
+function countHello(value) {
+    var count = 0;
+    var intervalId=setInterval(() => {
+        count++;
+    console.log('Hello', count);
+    if (count === value){
+        clearInterval(intervalId)
+    }
+},500);
+}
+
+countHello(5);
