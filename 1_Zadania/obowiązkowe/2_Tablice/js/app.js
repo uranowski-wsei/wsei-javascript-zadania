1d50b42004b02a79279abfecd799690890bc3b3c
Uranowski Antoni <antoni.uranowski@microsoft.wsei.edu.pl>
2019-10-06 10:50:59 +0200

    ex_3

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,3 +1,4 @@
+//zadanie 0
 function distFromAvarage(numberArray){
     var avg = 0;
     numberArray.forEach(element => {
@@ -15,3 +16,11 @@ function distFromAvarage(numberArray){
 }
 
 distFromAvarage([1,2,3,4,5,6,7])
+
+//zadanie 1
+let fruits = ['apple', 'orange', 'banana'];
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+for(let count = 0; count < fruits.length; count++){
+    console.log(fruits[count]);
+}
