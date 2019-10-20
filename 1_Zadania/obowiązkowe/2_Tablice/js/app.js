46c1ae662e9e12948f79684718d0dbc62d2bd40e
Uranowski Antoni <antoni.uranowski@microsoft.wsei.edu.pl>
2019-10-20 11:04:49 +0200

    arrays_ex_2

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -24,3 +24,59 @@ console.log(fruits[fruits.length - 1]);
 for(let count = 0; count < fruits.length; count++){
     console.log(fruits[count]);
 }
+//zadanie 3
+let array = ['apple', 'orange', 'banana'];
+function printTable(array){
+    for (let i=0; i< array.length ; i++)
+    console.log(array[i]);
+}
+//zadanie 4
+function multiply(array){
+    var temp = 1;
+    for(let i = 0; i<array.length; i++){
+        temp *=array[i];
+    }
+    console.log(temp);
+}
+//zadanie 5
+function getEvenAvarage(array){
+    let avg = 0;
+    let eventnumber = 0;
+    
+    array.forEach(
+        (element) =>{
+        if(element %2 == 0){
+            any += element;
+            eventnumber ++;
+        }
+    }
+    )
+    if (any == 0){
+        console.log(null);
+    }else{
+        console.log(avg);
+    }
+
+
+}
+
+//zadanie 6
+array = [145,11,3,64,4,6,10];
+function sortArray(array){
+    cons.t = array.sort((a,b)=>a-b);
+    cons(t);
+}
+//zadanie 7
+function addArrays(arr1, arr2){
+    var counter =  arr1.length>arr2.length? arr1.length:arr2.length;
+    var result = [];
+    for (var i = 0; i<counter; i++){
+        var firstNum = arr1[i] != null? arr1[i]:0;
+        var secondNum = arr2[i] != null? arr2[i]:0;
+        result[i] = firstNum+secondNum;
+    }
+    return result;
+
+
+    
+}
