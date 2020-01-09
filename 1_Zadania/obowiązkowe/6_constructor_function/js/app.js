00244f336e4ea7f5ce889cd57a7092df755ae829
uranowski-wsei <48623716+uranowski-wsei@users.noreply.github.com>
2020-01-10 00:45:14 +0100

    zadanie 6.0

---
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -0,0 +1,23 @@
+function Basket(){
+    this.products = [];
+    this.sum = 0;
+}
+
+Basket.prototype.addProduct = function(name, price){
+    this.products.push({
+        name: name,
+        price: price
+    })
+    this.sum += price;
+}
+
+Basket.prototype.showBasket = function(){
+    this.products.forEach(function(product){
+        console.log('Nazwa: ' + product.name + ' Cena: ' + product.price);
+    })
+    console.log('Do zapłaty: ' + this.sum);
+}
+
+
+
+
